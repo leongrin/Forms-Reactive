b5a1a091b2cf9d358a0f88ade5f7fd75269e6bd2
Leon Grin <41086608+leongrin@users.noreply.github.com>
2019-02-10 16:17:43 -0300

    Lesson 198 to 204

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {promise} from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,71 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenNames = ['Max', 'Suzan'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'username': new FormControl(null, [Validators.required, this.deniedName.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], [this.deniedEmails])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([]),
+    });
+
+/*    this.signupForm.valueChanges.subscribe((value) => {
+      console.log(value);
+    });*/
+
+    this.signupForm.statusChanges.subscribe((status) => {
+      console.log(status);
+    });
+
+/*    this.signupForm.setValue({
+      'userData': {
+        'username': 'Leon',
+        'email': 'test@test.com'
+      },
+      'gender': 'female',
+      'hobbies': []
+    });*/
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Mr. Leonardo'
+      }
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  deniedName(control: FormControl) {
+    if (this.forbiddenNames.includes(control.value)) {
+      return {'denied': true};
+    }
+    return null;
+  }
+
+  deniedEmails(control: FormControl) {
+    const promise = new Promise((resolve, reject) => setTimeout(
+      () => {
+        if (control.value === 'l@grinstein.com.br') {
+          resolve({'deniedEmail': true});
+        } else {
+          resolve(null);
+        }
+      }
+      , 3000));
+    return promise;
+  }
+
+
 }
